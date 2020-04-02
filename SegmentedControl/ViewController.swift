033a3cf794682b9f928f7aeb472fce312ab991ce
TaiYIChien <boss760801@gmail.com>
2020-04-02 18:07:04 +0800

    Initial Commit

---
+++ SegmentedControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegmentedControl
+//
+//  Created by Tai on 2020/4/2.
+//  Copyright © 2020 Tai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
